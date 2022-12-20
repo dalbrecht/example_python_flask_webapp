e0c698b477e1fc93455d3718c131043eb1edc63e
Don Albrecht <dalbrecht@dademmedia.com>
2022-12-20 11:27:30 -0500

    Initial Commit demonstrating how things interact between Flask Templates and Javascript functions.

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def welcome():  # put application's code here
+    return render_template(
+        'welcome.html',
+        title="Example Start Page",
+        description="This is an example start Page"
+    )
+
+
+@app.route('/details')
+def details():
+    return render_template(
+        'details.html'
+    )
+
+
+@app.route('/autocomplete.json')
+def autocomplete():
+    return json.dumps([
+        'red', 'blue', 'green', 'purple'
+    ])
 
 
 if __name__ == '__main__':
